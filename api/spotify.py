f8cbf8977367500ba71ad224f0f296492e151965
wg326 <57165451+wg326@users.noreply.github.com>
2023-02-14 02:20:55 -0800

    update

--- api/spotify.py
+++ api/spotify.py
@@ -170,4 +170,4 @@ def catch_all(path):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True, port=os.getenv("PORT") or 5000)
+    app.run(host="127.0.0.1", debug=True, port=os.getenv("PORT") or 5000)
